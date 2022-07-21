6960f7b44b39ebf733b602a57b6fd9f20b983559
K009 <spidahas@gmail.com>
2022-07-21 19:17:31 +0200

    Added profile image and improved description box colors.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.800','whiteAlpha.200')} p={3} mb={6} align="center">
                 Hello, I&apos;m a developer.
             </Box>
             
@@ -13,6 +13,23 @@ const Page = () => {
                     Dominik Chudy
                 </Heading>
                 <p>Developer / Designer</p>
+                </Box>
+                <Box 
+                    flexShrink={ 0 }
+                    mt={{ base: 4, md:0 }}
+                    ml={{ md:6 }}
+                    align="center"
+                >
+                    <Image 
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        borderRadius="full"
+                        maxWidth="100px"
+                        display="inline-block "
+                        src="/images/dominik.jpg"
+                        alt="Profile image"
+                    />
                 </Box>
             </Box>
         </Container>
