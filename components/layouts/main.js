8aa8f510ed329864be36be3ba3dc0f8b52a1471f
K009 <spidahas@gmail.com>
2022-07-30 12:10:07 +0200

    Added footer.

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Navbar from '../navbar'
+import Footer from '../footer'
 import { Box, Container } from '@chakra-ui/react'
 
 const Main = ({children, router}) => {
@@ -14,6 +15,7 @@ const Main = ({children, router}) => {
 
             <Container maxW="container.md" pt={14}>
                 {children }
+                <Footer />
             </Container>
         </Box>
     )
