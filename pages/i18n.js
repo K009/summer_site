d96d6519530d46571e731ed7824f3632b862143f
K009 <spidahas@gmail.com>
2022-08-01 17:08:26 +0200

    Language from i18next will be saved even on reload.

--- pages/i18n.js
+++ pages/i18n.js
@@ -33,9 +33,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "developer", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
+    lng: 'developer', //localStorage. getItem(key) || 'developer',
 
     interpolation: {
       escapeValue: false // react already safes from xss
