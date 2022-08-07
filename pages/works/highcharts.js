61dd7972effd9068456dda634f5b73e75fe10be2
K009 <spidahas@gmail.com>
2022-08-07 21:34:28 +0200

    Added highcharts site.

---
+++ pages/works/highcharts.js
@@ -0,0 +1,40 @@
+import { Container, List, ListItem, Badge, Link, Text } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+import { Title, WorkImage, Meta } from '../../components/work';
+import P from '../../components/paragraph';
+import Layout from '../../components/layouts/article';
+
+const Work = () => {
+    return (
+        <Layout title="Highcharts">
+            <Container mt={6}>
+                <Title>
+                    Highcharts
+                </Title>
+                <P>
+                    The Highcharts library has been built on JavaScript and TypeScript, it works with any back-end database or server stack. This is the place where my professional career began. Worked on bugfixes, features and also created plenty of custom charts to the clients. 
+                </P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                        <Link href="https://github.com/highcharts">
+                            https://github.com/highcharts <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Platform</Meta>
+                        <span>Windows/macOS/Linux/iOS/Android</span>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Stack</Meta>
+                        <span>Typescript</span>
+                    </ListItem>                                                         
+                </List>
+                <Text mb={4}>Example chart:</Text>
+                <WorkImage src="/images/highcharts2.png" alt="Highcharts" />
+            </Container>
+        </Layout>
+    )
+}
+
+export default Work;
